ca93f8c091bdc63fb490ddf08784680970564eec
Sacha <99975209+sduperret@users.noreply.github.com>
2024-10-25 13:09:50 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Ajoutez votre code JavaScript ici, par exemple :
+const button = document.querySelector("#hero button");
+
+button.addEventListener("click", () => {
+    alert("Bouton cliqué !");
+});
