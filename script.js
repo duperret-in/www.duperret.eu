8de96ac6cdf4e16b8f511bb3ee49becf7db2a811
Sacha <duperretsacha@gmail.com>
2024-10-25 19:02:49 +0200

    move folder to fix github action

--- script.js
+++
@@ -1,6 +0,0 @@
-// Ajoutez votre code JavaScript ici, par exemple :
-const button = document.querySelector("#hero button");
-
-button.addEventListener("click", () => {
-    alert("Bouton cliqué !");
-});
